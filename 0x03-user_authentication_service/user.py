34dc424f475f168de2917d0bdbc3418ef4ce0dc8
bm-197 <bisratmaru111@gmail.com>
2024-07-17 12:47:37 +0300

    [Task 0] feat: Task 1 completed

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" The user model's module.
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declartive_base
+
+
+Base = declartive_base()
+
+
+class User(Base):
+    """ Represents a record from `user` table
+    """
+    __tabelename__ = "user"
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=True)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
