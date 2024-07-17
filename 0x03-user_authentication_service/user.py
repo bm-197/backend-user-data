10ef0d0d28a2282bbddb9e5785c10e32ccba57d1
bm-197 <bisratmaru111@gmail.com>
2024-07-17 15:13:07 +0300

    [Task 1] update(user.py): corrected spelling error

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" The user model's module.
+"""The `user` model's module.
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -7,12 +7,14 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class User(Base):
-    """ Represents a record from `user` table
+    """Represents a record from the `user` table.
     """
-    __tablename__ = "user"
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
-    hashed_password = Column(String(250), nullable=True)
+    hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
+    
